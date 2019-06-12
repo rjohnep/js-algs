0a1a06b5386bac20d79ff69170f1c6c62f679783
Eugene Rogach <i.am.r.john@gmail.com>
2019-06-12 17:28:18 +0200

    Update bulbs.js

--- bulbs.js
+++ bulbs.js
@@ -2,22 +2,21 @@ const fixture = [2, 3, 1, 5, 4];
 
 const sumTo = (min, max) => (max - min + 1) * ((max+min) /2);
 const chainCheck = (arr) => {    
-    let controlSum = 0;
-    let arrSum = 0;
+  let controlSum = 0;
+  let arrSum = 0;
+  
+  return arr.reduce(
+    (acc, n, i) => {
+      let result = acc;
+      
+      controlSum = sumTo(0, i+1);
+      arrSum += n;
 
-    return arr.reduce(
-        (acc, n, i) => {
-            let result = acc;
-
-            controlSum = sumTo(0, i+1);
-            arrSum += n;
-
-            if(controlSum === arrSum) result++;
-
-            return result;
-        },
-        0
-    );
+      if(controlSum === arrSum) result++;
+        return result;
+      },
+      0
+  );
 };
 
 console.log(chainCheck(fixture));

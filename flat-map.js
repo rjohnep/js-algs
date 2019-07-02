e43cba1fe98761dba996767e4d4aff46beea2fba
Eugene Rogach <i.am.r.john@gmail.com>
2019-07-02 14:56:27 +0200

    Update flat-map.js

--- flat-map.js
+++ flat-map.js
@@ -1,3 +1,4 @@
+// #Recursion
 const deepFlatMapR = (arr) => {
   return arr.reduce((acc, i) => {
     if (Array.isArray(i)) {
@@ -13,6 +14,23 @@ const deepFlatMapR = (arr) => {
 assert.deepEqual(deepFlatMapR([1, 2, [3, [4], 5]]), [1, 2, 3, 4, 5]);
 
 
+// #Recursion with tail
+const deepFlatMap = (arr, tail = []) => {
+  return arr.reduce((acc, i) => {
+    if (Array.isArray(i)) {
+      return deepFlatMap(i, acc);
+    } else {
+      acc.push(i);
+    }
+
+    return acc;
+  }, tail);
+};
+
+assert.deepEqual(deepFlatMap([1, 2, [3, [4], 5]]), [1, 2, 3, 4, 5]);
+
+
+// #Iteratively
 const deepFlatMap = (arr) => {
   const result = [];
   let stack = arr;

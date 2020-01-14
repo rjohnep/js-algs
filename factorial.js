2aa73012d5d7844f5f19e372073a0f8e631b9e76
Eugene Rogach <eugene.rogach@pandadoc.com>
2020-01-14 23:16:10 +0100

    factorial is updated with least exposure style

--- factorial.js
+++ factorial.js
@@ -10,15 +10,23 @@ console.log(factorial(6)); // -> 720
 /**************************************/
 // WITCH CACHE
 
-const cache = {};
+const makeFactorialWithCache = () => {
+  const cache = {};
 
-const factorial = (x) => {
-  if (x < 2) return 1;
-  if (!(x in cache)) {
-    cache[x] = x * factorial(x - 1);
-  }
-  return cache[x];
-}
+  const factorial = (x) => {
+    if (x < 2) return 1;
+
+    if (!(x in cache)) {
+      cache[x] = x * factorial(x - 1);
+    }
+
+    return cache[x];
+  };
+
+  return factorial;
+};
+
+const factorial = makeFactorialWithCache();
 
 console.log(factorial(5)); // -> 120
 console.log(factorial(6)); // -> 720
